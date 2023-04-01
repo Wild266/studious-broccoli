72ba5ee13ffaf3a9851615cc423326d1e97ed0bb
Akshan Sameullah <akshansameullah@g.harvard.edu>
2023-04-01 18:51:04 -0400

    Did Novice

--- src/main/java/com/comp301/a07pizza/Cheese.java
+++ src/main/java/com/comp301/a07pizza/Cheese.java
@@ -1,8 +1,8 @@
 package com.comp301.a07pizza;
 
 public class Cheese extends IngredientImpl{
-  public static final Cheese MOZZARELLA =  new Cheese("mozzarella", true, true);
-  public static final Cheese BLEND =  new Cheese("blend", true, true);
+  public static final Cheese MOZZARELLA =  new Cheese("mozzarella", true, false);
+  public static final Cheese BLEND =  new Cheese("blend", true, false);
   public static final Cheese VEGAN =  new Cheese("vegan", true, true);
   private Cheese(String name, boolean isVegetarian, boolean isVegan) {
     super(name, isVegetarian, isVegan);
