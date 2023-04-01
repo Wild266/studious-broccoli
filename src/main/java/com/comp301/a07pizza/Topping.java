7401665d68d0e56392243416d5f23d5514c46441
Akshan Sameullah <akshansameullah@g.harvard.edu>
2023-04-01 18:54:49 -0400

    Did Novice

--- src/main/java/com/comp301/a07pizza/Topping.java
+++ src/main/java/com/comp301/a07pizza/Topping.java
@@ -1,23 +1,23 @@
 package com.comp301.a07pizza;
 
 public class Topping extends IngredientImpl{
-  private static final Topping TOMATO = new Topping("tomato", true, true);
-  private static final Topping GARLIC = new Topping("garlic", true, true);
-  private static final Topping MUSHROOMS = new Topping("mushrooms", true, true);
-  private static final Topping PINEAPPLE = new Topping("pineapple", true, true);
-  private static final Topping OLIVES = new Topping("olives", true, true);
-  private static final Topping GREEN_PEPPER = new Topping("green pepper", true, true);
-  private static final Topping SPINACH = new Topping("spinach", true, true);
-  private static final Topping ONION = new Topping("onion", true, true);
-  private static final Topping JALAPENO = new Topping("jalapeno", true, true);
-  private static final Topping SUN_DRIED_TOMATO = new Topping("sun-dried tomato", true, true);
-  private static final Topping PEPPERONI = new Topping("pepperoni", false, false);
-  private static final Topping GROUND_BEEF = new Topping("ground beef", false, false);
-  private static final Topping SAUSAGE = new Topping("sausage", false, false);
-  private static final Topping BACON = new Topping("bacon", false, false);
-  private static final Topping BUFFALO_CHICKEN = new Topping("buffalo chicken", false, false);
-  private static final Topping HAM = new Topping("ham", false, false);
-  private static final Topping ANCHOVIES = new Topping("anchovies", false, false);
+  public static final Topping TOMATO = new Topping("tomato", true, true);
+  public static final Topping GARLIC = new Topping("garlic", true, true);
+  public static final Topping MUSHROOMS = new Topping("mushrooms", true, true);
+  public static final Topping PINEAPPLE = new Topping("pineapple", true, true);
+  public static final Topping OLIVES = new Topping("olives", true, true);
+  public static final Topping GREEN_PEPPER = new Topping("green pepper", true, true);
+  public static final Topping SPINACH = new Topping("spinach", true, true);
+  public static final Topping ONION = new Topping("onion", true, true);
+  public static final Topping JALAPENO = new Topping("jalapeno", true, true);
+  public static final Topping SUN_DRIED_TOMATO = new Topping("sun-dried tomato", true, true);
+  public static final Topping PEPPERONI = new Topping("pepperoni", false, false);
+  public static final Topping GROUND_BEEF = new Topping("ground beef", false, false);
+  public static final Topping SAUSAGE = new Topping("sausage", false, false);
+  public static final Topping BACON = new Topping("bacon", false, false);
+  public static final Topping BUFFALO_CHICKEN = new Topping("buffalo chicken", false, false);
+  public static final Topping HAM = new Topping("ham", false, false);
+  public static final Topping ANCHOVIES = new Topping("anchovies", false, false);
 
   private Topping(String name, boolean isVegetarian, boolean isVegan) {
     super(name, isVegetarian, isVegan);
