6b92e01060f9560b74a6354374c27d7914373f78
Akshan Sameullah <akshansameullah@g.harvard.edu>
2023-04-02 16:57:58 -0400

    Did Jedi

--- src/main/java/com/comp301/a07pizza/PizzaFactory.java
+++ src/main/java/com/comp301/a07pizza/PizzaFactory.java
@@ -18,11 +18,7 @@ public class PizzaFactory {
     tps.add(Topping.create("ham"));
     tps.add(Topping.create("pineapple"));
     return new PizzaImpl(
-        size,
-        Crust.create("hand-tossed"),
-        Sauce.create("tomato"),
-        Cheese.create("blend"),
-        tps);
+        size, Crust.create("hand-tossed"), Sauce.create("tomato"), Cheese.create("blend"), tps);
   }
 
   public static Pizza makeMeatLoversPizza(Pizza.Size size) {
@@ -32,11 +28,7 @@ public class PizzaFactory {
     tps.add(Topping.create("bacon"));
     tps.add(Topping.create("ground beef"));
     return new PizzaImpl(
-        size,
-        Crust.create("deep-dish"),
-        Sauce.create("tomato"),
-        Cheese.create("blend"),
-        tps);
+        size, Crust.create("deep-dish"), Sauce.create("tomato"), Cheese.create("blend"), tps);
   }
 
   public static Pizza makeVeggieSupremePizza(Pizza.Size size) {
@@ -46,11 +38,7 @@ public class PizzaFactory {
     tps.add(Topping.create("mushrooms"));
     tps.add(Topping.create("olives"));
     return new PizzaImpl(
-        size,
-        Crust.create("thin"),
-        Sauce.create("tomato"),
-        Cheese.create("blend"),
-        tps);
+        size, Crust.create("thin"), Sauce.create("tomato"), Cheese.create("blend"), tps);
   }
 
   public static Pizza makeDailySpecialPizza() {
